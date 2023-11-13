a9ec1253ef0f6fc55bcd9de00ea852e1f40db871
LabeLyz <labelyz@outlook.com>
2023-11-13 23:58:25 +0800

    feat: 添加组件入口文件

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import type { App } from 'vue'
+
+import Button from '@/components/Button'
+import Collapse, { CollapseItem } from '@/components/Collapse'
+import Dropdown from '@/components/Dropdown'
+import Form, { FormItem } from '@/components/Form'
+import Icon from '@/components/Icon'
+import Message, { createMessage, closeAll as closeMessageAll } from '@/components/Message'
+import Input from '@/components/Input'
+import Select from '@/components/Select'
+import Switch from '@/components/Switch'
+import Tooltip from '@/components/Tooltip'
+
+const components = [
+  Button,
+  Collapse,
+  CollapseItem,
+  Dropdown,
+  Form,
+  FormItem,
+  Icon,
+  Message,
+  Input,
+  Select,
+  Switch,
+  Tooltip
+]
+
+const install = (app: App) => {
+  components.forEach((component) => {
+    app.component(component.name, component)
+  })
+}
+
+export {
+  install,
+  Button,
+  Collapse,
+  CollapseItem,
+  Dropdown,
+  Form,
+  FormItem,
+  Icon,
+  Message,
+  Input,
+  Select,
+  Switch,
+  Tooltip,
+  createMessage,
+  closeMessageAll
+}
+
+export default {
+  install
+}
